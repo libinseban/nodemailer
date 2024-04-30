6e5067eb7028be88713dd1a2b516d7d67818e36c
Libin Seban <libinseban97@gmail.com>
2024-04-30 14:18:44 +0530

    done

--- sendEmail.js
+++ sendEmail.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,  "index.html"));
+    res.sendFile(path.join(__dirname, "public" , "index.html"));
 });
 
 
