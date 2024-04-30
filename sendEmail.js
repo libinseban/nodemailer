73ddcabc0bc495bc9642cd0bdd4637cd4fd4047a
Libin Seban <libinseban97@gmail.com>
2024-04-30 13:26:08 +0530

    Add index.html for GitHub

--- sendEmail.js
+++ sendEmail.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodeMailer = require("nodemailer");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname,  "index.html"));
 });
 
 
